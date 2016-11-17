2edd2046e5d16112f1dd69a3feafd16d11247c1f
heorhi <heorhi.sidoryn@gmail.com>
2016-11-18 02:18:58 +0300

    add shelljs package

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express=require('express')
 var bodyParser = require('body-parser')
+var shell = require('shelljs');
 
 var app = express()
 app.use(bodyParser.json());
 
 app.post('/payload', function (req, res) {
   res.send(req.body);
-  require('simple-git')('/home/lasso-marko-startkit').pull();
+  require('simple-git')('/home/lasso-marko-startkit').pull(function(err, update){
+    if (update) {
+      console.log(update);
+      shell.exec('systemctl restart lasso');
+    }
+  });
 })
 
 app.listen(11111, function () {
